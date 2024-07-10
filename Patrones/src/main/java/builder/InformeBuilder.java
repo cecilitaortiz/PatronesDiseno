a4061078f1738fab01d3b8e89591e7cef9b697ba
Cecilia Ortiz <ceelorti@espol.edu.ec>
2024-07-10 16:07:43 -0500

    Creación

---
+++ Patrones/src/main/java/builder/InformeBuilder.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package builder;
+import principal.*;
+/**
+ *
+ * @author CltControl
+ */
+public class InformeBuilder {
+    protected Informe i;
+    
+    
+    public void construirNombre(){
+    //all
+    }
+    public void costruirContenido(){
+    //all
+    }
+
+    public Informe getInforme() {
+        return i;
+    }
+    
+}
